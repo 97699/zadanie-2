d41ee027a78af23f4109ef8b4899d95229d12c38
97699 <107889555+97699@users.noreply.github.com>
2022-06-20 23:06:27 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "kwa.h"
+#include "szes.h"
+
+int main()
+{
+    int war=0;
+    float a;
+    while(war<=3)
+    {
+        printf("Co chcesz zrobic?\n");
+        printf("1 pole kwadratu\n2 objetosc szescianu\n3 koniec\n\n");
+        scanf("%d", &war);
+        switch(war)
+        {
+            case 1:
+            {
+            printf("Podaj krawedz kwadratu:"); 
+            scanf("%f",&a); Polkwa(a);
+            break;
+            }
+            case 2: 
+            {
+            printf("Podaj krawedz szescianu:");
+            scanf("%f",&a);
+            Objszes(a);break;
+            }
+            case 3:
+            {
+            break;
+            }
+        }
+    }
+
+    return 0;
